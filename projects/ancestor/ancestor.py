241375f3626f39807abd5f58e7d0a010cdf18965
damonbogich <damonbogich@gmail.com>
2020-08-12 10:15:27 -0400

    MVP for ancestor

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -21,43 +21,33 @@ def get_parents(family, current_node):
 
 
 def earliest_ancestor(ancestors, starting_vertex):
-    #create an empty stack
+ 
         stack = Stack()
-        #create a set for visited vertices
+
         visited = set()
-        #add starting vertex path to stack [['1']]
+   
         stack.push([starting_vertex])
 
-        #while stack is not empty:
+
         while stack.size() > 0:
-            #pop off stack: stack = []
-            current_path = stack.pop() # --> ['6'] 
-            #store the last item from the popped off stack list: last = '1'
+            
+            current_path = stack.pop() 
+            
             current_paths_last_vertex = current_path[len(current_path) - 1] #6
-            #if last == destination_vertex:
             if current_paths_last_vertex not in visited:
                 parents = get_parents(ancestors, current_paths_last_vertex) # [3,5]
-                if parents == None:                               ### i think here we check 
-                                                                    #for parents current paths last vertex
-                                                                    #if it doesn't have any, return it
-                #return the node if it has no parents
+                if parents == [] and len(current_path) == 1: 
+                    return -1 
+                if parents == [] and stack.size() == 0:                               
                     return current_paths_last_vertex
-            #add last to visited: visited = {'6'}
                 visited.add(current_paths_last_vertex)
-                #add path to all of last's ('6') parents to stack:  [['6', '3'], ['6', '5']]
-
-                #deleted this line for now
-                ## parents = starting_vertex.get_parents(current_paths_last_vertex) # --> {'2', '3'}
-
-                #initialize list that can store both neighbor path lists
+                
                 neighbor_paths = []
-
-                #loop through parents and add them each to list with 1 ---> [['1', '2'], ['1', '3']]
                 for i in range(len(parents)):
                     neighbor_list = list(parents)
 
-                    neighbor_paths.append(current_path.copy()) #[['1'], ['1']]
-                    neighbor_paths[i].append(neighbor_list[i]) #[['1', '2'], ['1', '3']]
+                    neighbor_paths.append(current_path.copy()) 
+                    neighbor_paths[i].append(neighbor_list[i]) 
                     print('neighbor paths', neighbor_paths)
                 
                 for path in neighbor_paths:
